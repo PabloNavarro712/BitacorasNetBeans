91e19271e07b7f85130ea8a7b02f21c590d81730
PabloNavarro712 <7501@itescam.edu.mx>
2024-08-20 14:57:08 -0600

    Subir bitacoras

---
+++ src/main/java/com/mycompany/bitacoras/Bitacoras.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bitacoras;
+
+/**
+ *
+ * @author IGNITER
+ */
+public class Bitacoras {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
