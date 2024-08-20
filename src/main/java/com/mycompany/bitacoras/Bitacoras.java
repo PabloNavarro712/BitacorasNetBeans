e4b2ba455d2e674358d26102632005ab5732b918
JuanPabloNavarroDelgado <7501@itescam.edu.mx>
2024-08-20 15:52:50 -0600

    Prueba 2

--- src/main/java/com/mycompany/bitacoras/Bitacoras.java
+++ src/main/java/com/mycompany/bitacoras/Bitacoras.java
@@ -11,7 +11,9 @@ package com.mycompany.bitacoras;
 public class Bitacoras {
 
     public static void main(String[] args) {
-        System.out.println("Buenos diasssss");
+        System.out.println("Buenos diassss");
+        System.out.println("Buenas tardes");
+        System.out.println("Buenas noches");
     }
 }
 
